8a890958e81ddf5ac56a408715ef9e94372ca251
idirdev <idirdev@users.noreply.github.com>
2020-07-04 02:41:40 +0100

    fix: move utils to source, add vitest, import in tests

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,31 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
+
+export function formatDuration(seconds: number): string {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+  if (h > 0) return `${h}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
+  return `${m}:${String(s).padStart(2, '0')}`;
+}
+
+export function killsPerMinute(kills: number, durationSec: number): number {
+  if (durationSec <= 0) return 0;
+  return (kills / durationSec) * 60;
+}
+
+export function normalizeCoords(pos: { x: number; y: number }, bounds: { width: number; height: number }) {
+  return {
+    x: Math.max(0, Math.min(1, pos.x / bounds.width)),
+    y: Math.max(0, Math.min(1, pos.y / bounds.height)),
+  };
+}
+
+export function groupByRound<T extends { round: number }>(events: T[]): Record<number, T[]> {
+  const groups: Record<number, T[]> = {};
+  for (const e of events) {
+    (groups[e.round] ??= []).push(e);
+  }
+  return groups;
+}
--- tests/lib/utils.test.ts
+++ tests/lib/utils.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import { formatDuration, killsPerMinute, normalizeCoords, groupByRound } from '../../src/lib/utils';
 
 describe('telemetry utils', () => {
   it('formats duration in seconds', () => {
@@ -30,31 +31,3 @@ describe('telemetry utils', () => {
     expect(grouped[2]).toHaveLength(1);
   });
 });
-
-function formatDuration(seconds: number): string {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = seconds % 60;
-  if (h > 0) return `${h}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
-  return `${m}:${String(s).padStart(2, '0')}`;
-}
-
-function killsPerMinute(kills: number, durationSec: number): number {
-  if (durationSec <= 0) return 0;
-  return (kills / durationSec) * 60;
-}
-
-function normalizeCoords(pos: { x: number; y: number }, bounds: { width: number; height: number }) {
-  return {
-    x: Math.max(0, Math.min(1, pos.x / bounds.width)),
-    y: Math.max(0, Math.min(1, pos.y / bounds.height)),
-  };
-}
-
-function groupByRound<T extends { round: number }>(events: T[]): Record<number, T[]> {
-  const groups: Record<number, T[]> = {};
-  for (const e of events) {
-    (groups[e.round] ??= []).push(e);
-  }
-  return groups;
-}
